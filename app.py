f12f868659cb5bc27aa0619394aeecae3a779080
Ben Cornia <ben.cornia@gmail.com>
2021-08-12 19:19:23 -0600

    updated dynamic bend and flange display; added event bindings to each entry; added ability to display flat length

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import Frame, ttk
 from itertools import islice
-
+import math
 
 
 def main():
     # create top level widget
     root = tk.Tk()   
 
+    # create frames
     frm_main = tk.Frame(root)
     frm_main.master.title("Sheet Metal Calculator")
     frm_main.pack(padx=40, pady=10)
@@ -18,14 +19,69 @@ def main():
     frm_results = tk.Frame(root)
     frm_results.pack(padx=40, pady=10)
 
-    maths = create_entries(frm_main)
+    # return values as dictionaries
+    bend_properties = create_entries(frm_main)
     bend_count = create_combo(frm_main)
     bends = create_bends(frm_bend)
     flanges = create_flanges(frm_flg)
     results = display_results(frm_results)
     update_rows(flanges, bend_count, bends)
+    
+    return_results(bend_properties, bend_count, bends, flanges, results)
 
     root.mainloop()
+
+
+def return_results(bend_properties, bend_count, bends, flanges, results):
+    
+
+    def calc(event):
+
+        # calculate bend deduction
+        mt = float(bend_properties['Material Thickness'].get())
+        ir = float(bend_properties['Bend Radius'].get())
+        k = float(bend_properties['K Factor'].get())
+        bc = int(bend_count.get())
+
+        bend_deduct_list = []
+        for i in range(1, bc+1):
+            ba = float(bends[f'Bend Angle {i}'].children['!entry'].get())
+            ba = 180 - ba
+            bd = 2 * (math.tan(math.radians(ba/2)) * (ir + mt))- math.pi/180 * ba * (ir + k * mt)
+            bend_deduct_list.append(bd)
+
+        bend_deduction = sum(bend_deduct_list)
+
+        # calculate flat length
+        flange_list = []
+        for i in range(1, 6):
+            flange = float(flanges[f'Flange {i}'].children['!entry'].get())
+            flange_list.append(flange)
+
+        total_length = sum(flange_list) 
+        flat_length = total_length - bend_deduction
+
+        # display results
+        results['Flat Length:'].configure(text=f'{flat_length:.2f}')
+
+
+        
+
+    # bind entry objects to key release event
+    flanges['Flange 1'].children['!entry'].bind('<KeyRelease>', calc)
+    flanges['Flange 2'].children['!entry'].bind('<KeyRelease>', calc)
+    flanges['Flange 3'].children['!entry'].bind('<KeyRelease>', calc)
+    flanges['Flange 4'].children['!entry'].bind('<KeyRelease>', calc)
+    flanges['Flange 5'].children['!entry'].bind('<KeyRelease>', calc)
+
+    bends[f'Bend Angle 1'].children['!entry'].bind('<KeyRelease>', calc)
+    bends[f'Bend Angle 2'].children['!entry'].bind('<KeyRelease>', calc)
+    bends[f'Bend Angle 3'].children['!entry'].bind('<KeyRelease>', calc)
+    bends[f'Bend Angle 4'].children['!entry'].bind('<KeyRelease>', calc)
+
+    bend_properties['Material Thickness'].bind('<KeyRelease>', calc)
+    bend_properties['Bend Radius'].bind('<KeyRelease>', calc)
+    bend_properties['K Factor'].bind('<KeyRelease>', calc)
     
 
 def create_entries(frm_main):
@@ -89,6 +145,7 @@ def create_flanges(frm_flg):
 def create_bends(frm_bend):
     bend_count = 4
     bends = {}
+
     for i in range(bend_count):
         field_name = f'Bend Angle {i+1}'        
         row = tk.Frame(frm_bend)
@@ -110,9 +167,9 @@ def display_results(frm_results):
     labels = ('Flat Length:', 'Bend Centers:')
     results = {}
 
-    for label in labels:
+    for labelname in labels:
         row = tk.Frame(frm_results)
-        label = tk.Label(row, width=20, anchor='w', text=label)
+        label = tk.Label(row, width=20, anchor='w', text=labelname)
         label_val = tk.Label(row, width=17, anchor='w', text='0')
         row.pack(side=tk.TOP,
                  padx=5,
@@ -120,12 +177,12 @@ def display_results(frm_results):
         label.pack(side=tk.LEFT)
         label_val.pack(side=tk.RIGHT)
         # store values in dictionary
-        results[label]=label_val
+        results[labelname]=label_val
 
     return results
 
 
-def update_rows(flanges, bend_count, bends):
+def update_rows(flanges, bend_count, bends): 
 
 
     def update_flanges(event):
@@ -136,12 +193,18 @@ def update_rows(flanges, bend_count, bends):
         # unpack flange rows
         for row in flange_rows:
             row.pack_forget()
+            entry = row.children['!entry']
+            entry.delete(0, 'end')
+            entry.insert(0, '0')
 
         # pack flange rows
         for row in islice(flange_rows, flange_count):
             row.pack(side=tk.TOP,
                     padx=5,
                     pady=5)
+            entry = row.children['!entry']
+            entry.delete(0, 'end')
+            entry.insert(0, '0')
 
 
     def update_bends(event):
@@ -151,18 +214,23 @@ def update_rows(flanges, bend_count, bends):
         # unpack bend rows
         for row in bend_rows:
             row.pack_forget()
+            entry = row.children['!entry']
+            entry.delete(0, 'end')
+            entry.insert(0, '0')
 
         # pack bend rows
         for row in islice(bend_rows, bend_num):
             row.pack(side=tk.TOP,
                     padx=5,
-                    pady=5)
+                    pady=5)  
+            entry = row.children['!entry']
+            entry.delete(0, 'end')
+            entry.insert(0, '0')
 
 
-
-        
-
-    bend_count.bind('<<ComboboxSelected>>', update_bends)
+    bend_count.bind('<Visibility>', update_bends)
+    bend_count.bind('<Visibility>', update_flanges, add=True)
+    bend_count.bind('<<ComboboxSelected>>', update_bends, add=True)
     bend_count.bind('<<ComboboxSelected>>', update_flanges, add=True)
 
 
